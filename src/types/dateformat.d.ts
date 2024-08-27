6ae5683298eeac77b0938e1adb7c87eb28fe9bd2
Diyorbek <bekmurodovdiyorbek288@gmail.com>
2024-08-27 20:13:28 +0500

    salom

--- src/types/dateformat.d.ts
+++ src/types/dateformat.d.ts
@@ -2,4 +2,9 @@ declare module 'dateformat' {
     const dateFormat: any;
     export default dateFormat;
   }
+  // src/types/dateformat.d.ts yoki types/dateformat.d.ts
+declare module 'dateformat' {
+    const dateFormat: (date: Date | string | number, mask?: string, utc?: boolean, gmt?: boolean) => string;
+    export default dateFormat;
+  }
   

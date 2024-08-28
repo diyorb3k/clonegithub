7c0d524c4a397f98e8f6cce97cd7fb7ecfd1fccd
Diyorbek <bekmurodovdiyorbek288@gmail.com>
2024-08-28 12:32:27 +0500

    salom

--- src/app/.eslintrc.cjs
+++ src/app/.eslintrc.cjs
@@ -3,3 +3,25 @@ module.exports = {
       "@typescript-eslint/no-explicit-any": "error"
     }
   };
+
+
+
+
+
+
+  {
+    "parser": "@typescript-eslint/parser",
+    "plugins": [
+      "@typescript-eslint",
+      "react-hooks"
+    ],
+    "extends": [
+      "next/core-web-vitals",
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended"
+    ],
+    "rules": {
+      "@typescript-eslint/no-explicit-any": "error"
+    }
+  }
+  
